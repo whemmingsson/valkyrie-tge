e1553e44f1e6a9b6ece92726c7358f902646f5ec
William <william.hemmingsson@gmail.com>
2024-05-02 22:56:28 +0200

    Cleaned up shell logo and about section

--- source/engine/shell/ui/about.js
+++ source/engine/shell/ui/about.js
@@ -2,11 +2,17 @@ const logger = require('../../../core/io/logger.js');
 const about = {};
 
 about.display = () => {
-    const text = `About
+    const text = `
+Overview
+--------
 Valkyrie is a game engine designed to be simple and easy to use. It is built with JavaScript and Node.js, and is intended to be used for text-based games. Valkyrie provides a simple API for creating and managing game content, and includes a built-in shell for running and testing games.
 
-You are currently using the Valkyrie Shell. To run a game, place the game file in the 'games' directory and run the shell. The shell will automatically detect the game and run it.
+Running games
+-------------
+To run a game, place the game file in the 'games' directory and run the shell. The shell will automatically detect the game and run it.
 
+Building games
+--------------
 To build a game, you need to use the Valkyrie Compiler. The compiler reads game definitions (.json files) from the 'definitions' directory, validates the content and outputs a master JSON file that can be used by this shell. You can specify a different directory using the -d flag. For example: node index.js -d my-directory
 `;
     logger.default(text);
