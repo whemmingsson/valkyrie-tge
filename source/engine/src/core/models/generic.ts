0cdc6a3c5742152580f3fcc3dfbbb0aa9e8c8977
William Hemmingsson <william.hemmingsson@gmail.com>
2024-08-05 01:43:44 +0200

    Minor cleanup

--- source/engine/src/core/models/generic.ts
+++ source/engine/src/core/models/generic.ts
@@ -1,14 +1,8 @@
-import GameObject from "./gameObject.js";
 import TakeableObject from "./takeableObject.js";
 
 class Generic extends TakeableObject {
-    direction: any;
-    visible: any;
     constructor(source: any) {
         super(source);
-        this.direction = source.direction;
-        this.visible = source.visible;
-        this.events = source.events || [];
     }
 }
 
