c6924f28388262d00020a5d61373ef18ca90f531
William Hemmingsson <william.hemmingsson@gmail.com>
2024-08-05 02:07:25 +0200

    Trying to get prompt history to work. No luck so far.

--- source/engine/src/core/io/prompt.ts
+++ source/engine/src/core/io/prompt.ts
@@ -1,5 +1,7 @@
 import ps from "prompt-sync";
+import history from 'prompt-sync-history';
 
-const prompt = ps({ sigint: true });
+const prompt = ps({ sigint: true, history: history("hist", 10) });
+
 
 export default prompt;
