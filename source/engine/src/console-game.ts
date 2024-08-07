25de385befd42520cab4b6997c65e7ca0455e100
William Hemmingsson <william.hemmingsson@gmail.com>
2024-08-07 16:18:26 +0200

    Removed the old trigger resolver module

--- source/engine/src/console-game.ts
+++ source/engine/src/console-game.ts
@@ -1,6 +1,5 @@
 import logger from './core/io/logger.js';
 import buildMap from './world/map-builder.js';
-import TriggerResolver from './events/trigger-resolver.js';
 import eventManager from './events/event-manager.js';
 import Inventory from './core/models/inventory.js';
 import Context from './state/game-context.js';
@@ -14,7 +13,6 @@ import Debug from './debug.js';
 interface ConsoleGame {
     game: any;
     map: Map;
-    triggerResolver: TriggerResolver;
 }
 
 const ctx = Context.ctx;
@@ -23,7 +21,6 @@ class ConsoleGame {
     constructor(game) {
         this.game = game;
         this.map = buildMap(this.game.rooms);;
-        this.triggerResolver = new TriggerResolver(this.game);
 
         if (!this.game) {
             throw new Error('Game not initialized');
@@ -86,37 +83,18 @@ class ConsoleGame {
             const action = CommandResolver.resolve(command);
 
             if (!action) {
-                logger.warn(Translation.translate(Translation.INVALID_COMMAND_WARNING));
-                logger.empty();
-                continue;
-            }
-
-            if (typeof action === 'string') {
-                logger.warn(action);
+                logger.warn(Translation.translate(Translation.INVALID_COMMAND_WARNING) + "\n");
                 continue;
             }
 
             let actionResult = action();
 
-            // Why cant we just return the action directly? Do we really need to let it be triggered?
-            // Lets leave it for now, but we should consider refactoring this.
-
-            let tmp;
             while (actionResult !== null && actionResult !== undefined) {
-                tmp = this.triggerResolver.resolve(actionResult);
-                if (typeof tmp === 'function') {
-                    actionResult = tmp();
-                }
-                else if (typeof actionResult === 'function') {
+                if (typeof actionResult === 'function') {
                     actionResult = actionResult();
                 }
-                else if (typeof actionResult === 'string') {
-                    logger.warn(actionResult);
-                    break;
-                }
                 else {
-                    exitStatus = ExitStatus.ERROR;
-                    break;
+                    logger.warn("actionResult is not a function");
                 }
             }
 
