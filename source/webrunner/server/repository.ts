73d86e6458259cbdcd9ad78974e405b00cca87bb
William Hemmingsson <william.hemmingsson@gmail.com>
2024-09-11 21:57:09 +0200

    Fixed proper fetching of available games

--- source/webrunner/server/repository.ts
+++ source/webrunner/server/repository.ts
@@ -1,6 +1,12 @@
 import { getAvailableGames } from '../../engine/src/gameLoader.js';
+import path from 'path';
 
 export const getAllGames = () => {
-    const games = getAvailableGames();
-    return games;
+    const gameFiles = getAvailableGames();
+    const games = gameFiles.map(gameFile => {
+        return {
+            name: path.parse(gameFile).name,
+            description: 'Feature not implemented yet'
+        }
+    });
 }
